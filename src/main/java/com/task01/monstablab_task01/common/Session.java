8569c233de31a80975fa53c918a0beda587ef13b
trongdat1998 <68001618+trongdat1998@users.noreply.github.com>
2021-09-22 23:22:37 +0700

    1

--- src/main/java/com/task01/monstablab_task01/common/Session.java
+++
@@ -1,7 +0,0 @@
-package com.task01.monstablab_task01.common;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class Session {
-}
--- src/main/java/com/task01/monstablab_task01/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.task01.monstablab_task01.service;
-
-import com.task01.monstablab_task01.entity.Account;
-
-import java.util.List;
-public interface AccountService {
-    public  List<Account> findAll() ;
-}
